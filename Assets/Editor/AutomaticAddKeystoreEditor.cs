3821350bd9890d16110f9319caf99af826665320
armariya <arm.armariya@gmail.com>
2017-07-06 16:28:57 +0700

    Add AutomaticAddKeystoreEditor

---
+++ Assets/Editor/AutomaticAddKeystoreEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public class AutomaticAddKeystoreEditor {
+
+	public const string KEY_STORE_NAME = "Assets/Keystore/debug.keystore";
+	public const string KEY_STORE_PASSWORD = "android";
+	public const string KEY_ALIAS_NAME = "androiddebugkey";
+	public const string KEY_ALIAS_PASSWORD = "android";
+
+	static AutomaticAddKeystoreEditor() {
+		PlayerSettings.Android.keystoreName = KEY_STORE_NAME;
+		PlayerSettings.Android.keystorePass = KEY_STORE_PASSWORD;
+		PlayerSettings.Android.keyaliasName = KEY_ALIAS_NAME;
+		PlayerSettings.Android.keyaliasPass = KEY_ALIAS_PASSWORD;
+	}
+}
