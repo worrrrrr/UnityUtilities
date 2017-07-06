b7b4c1e35ca810b7d58815ed237cab7604a24f46
armariya <arm.armariya@gmail.com>
2017-07-06 16:10:07 +0700

    Add OpenSceneEditor

---
+++ Assets/Editor/OpenSceneEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class OpenSceneEditor : MonoBehaviour {
+	[MenuItem( "Open Scene/Main" )]
+	public static void OpenLoginScene() {
+		OpenScene("Main");
+	}
+
+	static void OpenScene(string sceneName) {
+		if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() == true) {
+			EditorSceneManager.OpenScene("Assets/Scenes/" + sceneName + ".unity");
+		}
+	}
+}
