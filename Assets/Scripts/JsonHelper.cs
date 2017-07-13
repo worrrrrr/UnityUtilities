974a4cab6caa90e24c2b9cbe83d5713bb0f924dd
armariya <arm.armariya@gmail.com>
2017-07-13 16:40:55 +0700

    Add JsonHelper

---
+++ Assets/Scripts/JsonHelper.cs
@@ -0,0 +1,19 @@
+﻿// From https://forum.unity3d.com/threads/how-to-load-an-array-with-jsonutility.375735/
+// Author: ffleurey
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonHelper : MonoBehaviour {
+	public T[] FromJsonArray<T>(string json) {
+		string newJson = string.Format("{\"array\": {0}}", json);
+		Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>> (newJson);
+		return wrapper.array;
+	}
+
+	[System.Serializable]
+	public class Wrapper<T> {
+		public T[] array;
+	}
+}
